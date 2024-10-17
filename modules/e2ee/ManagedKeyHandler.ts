eed4aea9379e19996d3acef015d439cf80b2bb7a
Internxt <internxt@Internxts-MacBook-Air.local>
2024-10-17 14:12:00 +0200

    change debug message

--- modules/e2ee/ManagedKeyHandler.ts
+++ modules/e2ee/ManagedKeyHandler.ts
@@ -96,7 +96,7 @@ export class ManagedKeyHandler extends KeyHandler {
         try {
             this._onKeyGeneration();
             await this._olmAdapter.initSessions();
-            console.log('CHECK: no key update before that!!!!!!');
+            console.log('CHECK: all olm sessions should be established now!!!!!!');
             const mediaKeyIndex = await this._olmAdapter.updateKey(this._olmKey, this._pqKey);
                 
             // Set our key so we begin encrypting.
