a912e9a86fa5d87f2a1eb8031e67930b5f73b1d9
tamarafinogina <tamara@internxt.com>
2025-10-20 18:53:59 +0200

    fix build

--- webpack-shared-config.js
+++ webpack-shared-config.js
@@ -52,8 +52,8 @@ module.exports = (minimize, analyzeBundle) => {
         },
         performance: {
             hints: minimize ? 'error' : false,
-            maxAssetSize: 2 * 1024 * 1024,
-            maxEntrypointSize: 2 * 1024 * 1024
+            maxAssetSize: 1.75 * 1024 * 1024,
+            maxEntrypointSize: 1.75 * 1024 * 1024
         },
         plugins: [
             new IgnorePlugin({ resourceRegExp: /^(@xmldom\/xmldom|ws)$/ }),
