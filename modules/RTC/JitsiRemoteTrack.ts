3d2779adc7a09ee18f5c5b81a520f51be219ca3b
tamarafinogina <tamara@internxt.com>
2026-08-25 12:18:37 +0200

    fix typo

--- modules/RTC/JitsiRemoteTrack.ts
+++ modules/RTC/JitsiRemoteTrack.ts
@@ -508,13 +508,13 @@ export default class JitsiRemoteTrack extends JitsiTrack {
                 try {
                     // check wether the canvas must be changed
                     if (canvasEncoded.width !== nwidth || canvasEncoded.height !== nheight) {
-                        console.log('Decoder: changeing encoding canvas from:', canvasEncoded.width, 'x', canvasEncoded.height, 'to', nwidth, 'x', nheight);
+                        console.log('Decoder: changing encoding canvas from:', canvasEncoded.width, 'x', canvasEncoded.height, 'to', nwidth, 'x', nheight);
                         canvasEncoded.width = nwidth;
                         canvasEncoded.height = nheight;
                     }
 
                     if (canvasDecoded.width !== nwidth * 2 || canvasDecoded.height !== nheight * 2) {
-                        console.log('Decoder: changeing decoded canvas from:', canvasDecoded.width, 'x', canvasDecoded.height, 'to', nwidth * 2, 'x', nheight * 2);
+                        console.log('Decoder: changing decoded canvas from:', canvasDecoded.width, 'x', canvasDecoded.height, 'to', nwidth * 2, 'x', nheight * 2);
                         canvasDecoded.width = nwidth * 2;
                         canvasDecoded.height = nheight * 2;
                     }
